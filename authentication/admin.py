805ca2118a34d49a8a4f550a9d94c9f08da71faf
Ahsan Naveed <ahsannaveed94@gmail.com>
2020-02-15 17:24:08 +0500

    resolved m erge conflicts

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from .models import CustomerProfile, ChefProfile
+from management.models import Shift
 
 User = get_user_model()
 
 admin.site.unregister(User)
 
 
-class ProfileAdmin(admin.ModelAdmin):
+class ShiftInline(admin.StackedInline):
+    model = Shift
+
+
+class ChefAdmin(admin.ModelAdmin):
+    search_fields = ('user__email',)
+    list_display = ('__str__', 'email')
+    inlines = [ShiftInline]
+
+
+class CustomerAdmin(admin.ModelAdmin):
     search_fields = ('user__email',)
     list_display = ('__str__', 'email')
 
 
-admin.site.register(CustomerProfile, ProfileAdmin)
-admin.site.register(ChefProfile, ProfileAdmin)
+admin.site.register(CustomerProfile, CustomerAdmin)
+admin.site.register(ChefProfile, ChefAdmin)
